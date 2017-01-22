05a0ac95887180e17065df09f7d4b43873f538e0
rob <rob@unx.pl>
2017-01-22 01:15:02 +0100

    set timezone in runtime

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if [ -n "$TZ" ] && [ -e /usr/share/zoneinfo/$TZ ]; then
+	echo "$TZ" > /etc/timezone
+	dpkg-reconfigure -f noninteractive tzdata
+fi
+
 confd -onetime -backend env
 exec nginx -g "daemon off;"
